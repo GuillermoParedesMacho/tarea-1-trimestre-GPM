e9e818c4063ebc8f84c0fd38d3e77fb1d1c34eed
Guillermo Paredes Macho <2c62b044cbc469601ae3bdade3f7c7b2db07f3ab>
2020-11-19 19:16:34 +0000

    Initial Commit

---
+++ tarea-1-trimestre-GPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarea-1-trimestre-GPM
+//
+//  Created by user177872 on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

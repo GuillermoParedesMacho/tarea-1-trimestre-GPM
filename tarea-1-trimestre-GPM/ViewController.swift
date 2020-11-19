8868b7466246f0cf729675655332dbc8f3ca7c5d
Guillermo Paredes Macho <2c62b044cbc469601ae3bdade3f7c7b2db07f3ab>
2020-11-19 19:56:09 +0000

    clear develop

--- tarea-1-trimestre-GPM/ViewController.swift
+++ tarea-1-trimestre-GPM/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //indicadores pantalla
+    @IBOutlet weak var act_num_lb: UILabel!
+    @IBOutlet weak var left_num_lb: UILabel!
+    @IBOutlet weak var left_cards_lb: UILabel!
+    @IBOutlet weak var right_num_lb: UILabel!
+    @IBOutlet weak var right_cards_lb: UILabel!
+    
+    //botones
+    @IBAction func left_num_bt(_ sender: UIButton) {
+        act_num_lb.text = "left";
+    }
+    @IBAction func right_num_bt(_ sender: UIButton) {
+        act_num_lb.text = "right";
+    }
+    
+    //inicio app
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    //funciones aplicacion
+    //funciones de inicio
+    func genera_cartas(num_cart: Int, cantidad: Int){
+        //introducir num carta y cuantas de ese numero
+    }
+    
+    //funciones de juego
 
 }
 
